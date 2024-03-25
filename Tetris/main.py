797706ed57d2027691934c6b948c6e1407f2f0ca
Jules BR <94800084+Jules-Boillon-Rougeaux@users.noreply.github.com>
2024-03-25 13:10:20 +0100

    Demonstration to Aymerick

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,4 +1,4 @@
-import pygame
+from player import *
 
 
 class Game:  # Game class
@@ -6,6 +6,7 @@ class Game:  # Game class
         self.screen = screen_display
         self.running = True
         self.clock = pygame.time.Clock()
+        self.player = Player(0, 0)
 
     def handling_events(self):
         for event in pygame.event.get():
@@ -24,17 +25,21 @@ class Game:  # Game class
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:  # Right arrow
-            pass
+            self.player.velocity[0] = 1
         if keys[pygame.K_DOWN]:  # Down arrow
-            pass
+            self.player.velocity[1] = 1
         if keys[pygame.K_LEFT]:  # Left arrow
-            pass
+            self.player.velocity[0] = -1
+        if keys[pygame.K_UP]:  # Left arrow
+            self.player.velocity[1] = -1
+
 
     def update(self):
-        pass
+        self.player.move()
 
     def display(self):
         self.screen.fill("white")
+        self.player.draw(self.screen)
         pygame.display.update()
 
     def run(self):
@@ -49,7 +54,7 @@ class Game:  # Game class
 
 pygame.init()  # initialisation des modules pygame
 screen = pygame.display.set_mode((1080, 720))  # screen de 1080 pixel sur 720
-pygame.display.set_caption("T'triste")
+pygame.display.set_caption("Tetris")
 pygame.display.set_icon(pygame.image.load("images/logo.png"))
 game = Game(screen)
 game.run()
